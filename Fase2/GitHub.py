4e1a146de9b12bd8d56859c455572f9ea53ba612
Felipe Sena <felipedepaulasena@hotmail.com>
2018-11-22 00:18:47 -0200

    Métodos para utilização da API

--- Fase2/GitHub.py
+++ Fase2/GitHub.py
@@ -1,68 +1,237 @@
+from github import Github as g
 import requests
 import json
 import time
-from requests.auth import HTTPBasicAuth
 
 DEFAULT_URL = "https://api.github.com"
+JSON_FILE = "repo_infos.json"
 
 
 class Repo:
     def __init__(self, owner, name):
         self.owner = owner
         self.name = name
+        self.quantity_pullrequests = 0
+        self.quantity_forks = 0
+        self.quantity_stars = 0
+        self.quantity_commits = 0
+
+    def to_dict(self):
+        return{
+            'owner':self.owner,
+            'name':self.name,
+            'pullrequests':self.quantity_pullrequests,
+            'forks':self.quantity_forks,
+            'stars':self.quantity_stars,
+            'commits':self.quantity_commits
+        }
 
 
 class GitHub:
-    def __init__(self, repo):
+    def __init__(self, repo, token):
         self.repo = repo
+        self.git = g(token)
+        self.repository = self.git.get_repo(f'{self.repo.owner}/{self.repo.name}')
+
+    def make_analisys_api(self):
+        self.get_infos_api()
+
+        file = open(JSON_FILE, 'w')
+        json.dump(self.repo.__dict__, file)
+        file.close()
+
+    def get_infos_api(self):
+        print('Starting getting repo informations using API...')
+        self.get_stars_api()
+        self.get_forks_api()
+        self.get_pullrequests_api()
+        self.get_commits_api()
+
+    def get_commits_api(self):
+        print('Starting getting number of commits using API...')
+        commits = self.repository.get_commits()
+
+        page = 1
+        number_commits = len(commits.get_page(page))
+        self.repo.quantity_commits += number_commits
+        page += 1
+
+        while number_commits != 0:
+            time.sleep(9)
+            number_commits = len(commits.get_page(page))
+            self.repo.quantity_commits += number_commits
+            page += 1
+
+    def get_pullrequests_api(self):
+        print('Starting getting number of pullrequests using API...')
+        pulls = self.repository.get_pulls(state='all')
+
+        page = 1
+        number_pulls = len(pulls.get_page(page))
+        self.repo.quantity_pullrequests += number_pulls
+        page += 1
+
+        while number_pulls != 0:
+            time.sleep(9)
+            page = 1
+            number_pulls = len(pulls.get_page(page))
+            self.repo.quantity_pullrequests += number_pulls
+            page += 1
+
+    def get_forks_api(self):
+        print('Starting getting number of forks using API...')
+        self.repo.quantity_forks = self.repository.forks_count
+
+    def get_stars_api(self):
+        print('Starting getting number of stars using API...')
+        self.repo.quantity_stars = self.repository.stargazers_count
 
     def make_analisys(self):
+        self.get_infos()
+
+        file = open(JSON_FILE, 'w')
+        json.dump(self.repo, file)
+        file.close()
+
+    def get_infos(self):
+        print('Starting getting repo informations...')
+        self.get_stars()
+        self.get_forks()
+        self.get_pullrequests()
+
+    def get_stars(self):
+        print('Starting getting number of stars...')
+
+        self.verify_ratelimit()
+
+        url = DEFAULT_URL + f'/repos/{self.repo.owner}/{self.repo.name}/stargazers?per_page=100'
+
+        response = requests.get(url, auth=self.auth)
+
+        if response.status_code == 200:
+            json_response = json.loads(response.text)
+
+            if json_response:
+                print('+ {} stars - {}/{}'.format(len(json_response), self.repo.owner, self.repo.name))
+                self.repo.quantity_forks += len(json_response)
+
+        while 'next' in response.links:
+            time.sleep(9)
+            response = requests.get(response.links['next']['url'], auth=self.auth)
+
+            if response.status_code == 200:
+                print('+ {} stars - {}/{}'.format(len(json_response), self.repo.owner, self.repo.name))
+                self.repo.quantity_forks += len(json.loads(response.text))
+            elif response.status_code == 403:
+                while response.status_code != 200:
+                    response = self.deal_abuse(response)
+
+                if response.status_code == 200:
+                    print('+ {} stars - {}/{}'.format(len(json_response), self.repo.owner, self.repo.name))
+                    self.repo.quantity_forks += len(json.loads(response.text))
+                    print('Continuing with the requests...')
+            else:
+                break
+
+    def get_forks(self):
+        print('Starting getting number of forks...')
+
+        self.verify_ratelimit()
+        url = DEFAULT_URL + f'/repos/{self.repo.owner}/{self.repo.name}/forks?per_page=100'
+
+        response = requests.get(url, auth=self.auth)
+
+        if response.status_code == 200:
+            json_response = json.loads(response.text)
+
+            if json_response:
+                print('+ {} forks - {}/{}'.format(len(json_response), self.repo.owner, self.repo.name))
+                self.repo.quantity_forks += len(json_response)
+
+        while 'next' in response.links:
+            time.sleep(9)
+            response = requests.get(response.links['next']['url'], auth=self.auth)
+
+            if response.status_code == 200:
+                print('+ {} forks - {}/{}'.format(len(json_response), self.repo.owner, self.repo.name))
+                self.repo.quantity_forks += len(json.loads(response.text))
+            elif response.status_code == 403:
+                while response.status_code != 200:
+                    response = self.deal_abuse(response)
+
+                if response.status_code == 200:
+                    print('+ {} forks - {}/{}'.format(len(json_response), self.repo.owner, self.repo.name))
+                    self.repo.quantity_forks += len(json.loads(response.text))
+                    print('Continuing with the requests...')
+            else:
+                break
+
+    def deal_abuse(self, response):
+        retry_time = int(response.headers['Retry-after'])
+        print('Triggered an abuse detection. Retry after {} s'.format(retry_time))
+        time.sleep(retry_time)
+        response = requests.get(response.url, auth=self.auth)
+
+        return response
+
+    def get_pullrequests(self):
+        print('Starting getting number of pullrequests...')
+
         have_content = True
-        page_number = 1       
-        parameters = {'state':'all'}
+        page_number = 1
+        parameters = {'state': 'all'}
 
         while have_content:
             self.verify_ratelimit()
 
-            url = DEFAULT_URL + ('/repos/%s/%s/pulls?page=%d&per_page=100' % (self.repo.owner, self.repo.name, page_number))
+            url = DEFAULT_URL + f'/repos/{self.repo.owner}/{self.repo.name}/pulls?page={page_number:d}&per_page=100'
 
-            response = requests.get(url, auth=HTTPBasicAuth('<user>', '<pass>'), params=parameters)
+            time.sleep(9)
+
+            response = requests.get(url, auth=self.auth, params=parameters)
 
             if response.status_code == 200:
                 json_response = json.loads(response.text)
 
-                print('Repo: {} / N° Pull Requests: {}'.format(self.repo.name, len(json_response)))
-
                 if not json_response:
                     have_content = False
                     continue
-                
+
+                print('+ {} pullrequests - {}/{}'.format(len(json_response), self.repo.owner, self.repo.name))
+                self.repo.quantity_pullrequests += len(json_response)
+
                 page_number += 1
-            
+            elif response.status_code == 403:
+                while response.status_code != 200:
+                    response = self.deal_abuse(response)
+
+                if response.status_code == 200:
+                    print('+ {} pullrequests - {}/{}'.format(len(json_response), self.repo.owner, self.repo.name))
+                    self.repo.quantity_forks += len(json.loads(response.text))
+                    page_number += 1
+                    print('Continuing with the requests...')
 
     def verify_ratelimit(self):
         url = DEFAULT_URL + '/rate_limit'
-        response = requests.get(url, auth=HTTPBasicAuth('<user>','<pass>'))
+        response = requests.get(url, auth=self.auth)
 
         if response.status_code == 200:
             json_response = json.loads(response.text)
             remaining = json_response['resources']['core']['remaining']
             if remaining == 0:
-                mins = 0
+                minutes = 0
                 secs = 59
                 print('Você chegou ao limite de requisições. Esperando até que possamos continuar o trabalho... \r')
 
-                while mins != 60:
-                    print('Falta {} min e {:2d} sec para continuar as requisições'.format(60 - mins, secs))
+                while minutes != 60:
+                    print('Falta {} min e {:2d} sec para continuar as requisições'.format(60 - minutes, secs))
 
                     time.sleep(1)
                     secs -= 1
 
                     if secs == 0:
                         secs = 59
-                        mins += 1
-            else:
-                print('Você ainda tem {} requisições para se fazer'.format(remaining))
+                        minutes += 1
 
 
 class PullRequest:
--- Fase2/main.py
+++ Fase2/main.py
@@ -2,7 +2,7 @@ from Fase2 import GitHub as gh
 from multiprocessing.dummy import Pool
 
 
-def top10Repos():
+def top10repos():
     repos = []
 
     repos.append(gh.Repo('Microsoft', 'vscode'))
@@ -12,19 +12,19 @@ def top10Repos():
     repos.append(gh.Repo('MicrosoftDocs', 'azure-docs'))
     repos.append(gh.Repo('angular', 'angular'))
     repos.append(gh.Repo('ansible', 'ansible'))
-    repos.append(gh.Repo('kubernates', 'kubernates'))
+    repos.append(gh.Repo('kubernetes', 'kubernetes'))
     repos.append(gh.Repo('npm', 'npm'))
     repos.append(gh.Repo('DefinitelyTyped', 'DefinitelyTyped'))
 
     return repos
 
 
-def processRepo(repo):
-    github = gh.GitHub(repo)
-    github.make_analisys()
+def process_repo(repo):
+    github = gh.GitHub(repo, '25d9bcaf7160e2f2eec7969d7b893b5729701e63')
     print('{}/{}'.format(repo.owner, repo.name))
+    github.make_analisys_api()
 
 
 if __name__ == '__main__':
     pool = Pool(10)
-    results = pool.map(processRepo, top10Repos())
+    results = pool.map(process_repo, top10repos())
